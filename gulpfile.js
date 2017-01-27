5b076b418dc0a7c0c1dbda0b47843b713950088e
Erol Zivina <erol@wmtek.com>
2017-01-27 16:20:55 +0100

    First Commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+
+// my-log - something we name
+gulp.task('my-log', function(){
+
+	gutil.log('Workflows are awesome!');
+
+});
+
+
+
+//ccc
