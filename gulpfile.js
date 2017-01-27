b095c98324ae05c8d6d97a50b2eac7354326d7e9
Erol Zivina <erol@wmtek.com>
2017-01-27 16:42:46 +0100

    Added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
 
 // my-log - something we name
-gulp.task('my-log', function(){
+//gulp.task('my-log', function(){
+//	gutil.log('Workflows are awesome!');
+//});
 
-	gutil.log('Workflows are awesome!');
+//var coffeeSources = ['components/coffee/*.coffee']; // include all
+var coffeeSources = ['components/coffee/tagline.coffee'];
 
+gulp.task('coffee-test', function(){
+	gulp.src(coffeeSources)
+		.pipe(coffee({bare: true})
+			.on('error', gutil.log))
+		.pipe(gulp.dest('components/scripts'));
 });
